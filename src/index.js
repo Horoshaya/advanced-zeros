8ad491ac56874ec07ed5c0a15c9703d89800f5ee
Дарья <daria@MBP-Dara.lan>
2018-09-20 00:31:27 +0300

    almost done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	var j = 1; 
+		i = 2, 
+		zeros = 0,
+		maxZeros = [],
+		primes = [];
+
+	do { 
+		if (base % i == 0 && base != 1) { 
+		
+			primes[j-1] = i; 
+			j++; 
+			base = base / i; 
+
+		} else { 
+			i++; 
+		} 
+
+	} while (i < base); 
+		primes[j-1] = i; 
+
+	var degree = 1; 
+	
+	primes.forEach(function(item, i, a) { 
+		while (number/Math.pow(primes[i], degree) > 1) { 
+			
+			zeros += Math.floor(number / Math.pow(primes[i], degree)); 
+			degree++; 
+		} 
+		
+		maxZeros.push(zeros); 
+		zeros = 0; 
+		degree = 1; 
+	}); 
+
+	var count = 1; 
+
+	primes.forEach(function(item, i, a) { 
+		if (i < primes.length -1) { 
+			
+			if (primes[i] == primes[i+1]) { 
+				count++; 
+
+			} else { 
+				maxZeros[i] = Math.floor(maxZeros[i]/count); 
+				count = 1; 
+			} 
+		} else { 
+			maxZeros[i] = Math.floor(maxZeros[i]/count); 
+			count = 1; 
+		} 
+	}); 
+
+	var minZeros = maxZeros[0];
+
+	maxZeros.forEach(function(h, i, maxZeros) {
+	    if (i < maxZeros.length -1) { 
+            if (maxZeros[i] <= maxZeros[i+1]) { 
+                minZeros = maxZeros[i];
+            }
+	    } else {
+	        if (maxZeros[i] < minZeros) {
+	            minZeros = maxZeros[i];
+	        }
+	    }            
+	});
+
+
+	return minZeros;
 }
